00e75b9eb2dd83639d54f1226d40b454c88b67a9
sevimzini <sevim.zini@gmail.com>
2020-06-03 00:14:42 -0400

    first commit

---
+++ src/test/java/features/FeatureRunner.java
@@ -0,0 +1,17 @@
+package features;
+
+
+import com.intuit.karate.junit5.Karate;
+
+public class FeatureRunner {
+
+    @Karate.Test
+    Karate metaWeatherTest(){
+        return new Karate().tags("@meta_weather").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate bookIt(){
+        return new Karate().tags("@bookit").relativeTo(getClass());
+    }
+}
